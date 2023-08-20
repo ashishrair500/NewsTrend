49110a6f5cf041bce83086cc77cffceabfcd2401
ashishrair500 <100395176+ashishrair500@users.noreply.github.com>
2023-08-20 16:03:37 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -30,7 +30,7 @@ const News = (props)=>{
     }
 
     useEffect(() => {
-        document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
+        document.title = `${capitalizeFirstLetter(props.category)} - NewsTrend`;
         updateNews(); 
         // eslint-disable-next-line
     }, [])
@@ -48,7 +48,7 @@ const News = (props)=>{
  
         return (
             <>
-                <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
+                <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>NewsTrend- Top {capitalizeFirstLetter(props.category)} Headlines</h1>
                
                 <InfiniteScroll
                     dataLength={articles.length}
